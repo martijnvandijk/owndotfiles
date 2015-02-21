54748bce23e446ae3d622c252b33c2a7072689c0
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2015-02-21 20:40:21 +0100

    updated install.sh for new sublime text situation

--- sublime3/install.sh
+++ sublime3/install.sh
@@ -6,5 +6,6 @@ sublime_dir=$HOME/.config/sublime-text-3
 dotfiles=$HOME/.dotfiles
 mv "$sublime_dir" "$sublime_dir.backup"
 mkdir -p $HOME/.config/sublime-text-3/ # in case the st3 directory doesn't exist (yet)
-ln -s "$dotfiles/sublime3/Installed Packages" "$sublime_dir/Installed Packages"
-ln -s "$dotfiles/sublime3/Packages/" "$sublime_dir/Packages"
+#ln -s "$dotfiles/sublime3/Installed Packages" "$sublime_dir/Installed Packages"
+ln -s "$dotfiles/sublime3/Packages/User" "$sublime_dir/Packages/User"
+echo "Don't forget to install the package control package in Sublime Text! It wil install and update all your packages"
